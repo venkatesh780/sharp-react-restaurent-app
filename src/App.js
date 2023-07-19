a28dd2ddd8c3bdf4e53c33a02b5c5b93d635919d
Venkatesh Thimmaiah <venkatesh131780@gmail.com>
2023-07-19 12:03:09 +0530

    added meal list into the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Header from "./components/Layout/Header";
-
-import MealsSummary from "./components/Meals/MealsSummary";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <MealsSummary />
+        <Meals />
       </main>
     </>
   );
